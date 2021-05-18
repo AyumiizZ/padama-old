1b23c07553d1113bafcf6d2138c08b9218aa26d0
AyumiizZ <korrawitc@gmail.com>
2021-05-18 17:26:35 +0700

    ➖ body-parser

--- back/src/app.js
+++ back/src/app.js
@@ -1,12 +1,12 @@
-console.log('Hello')
+// console.log('Hello')
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('conbine'))
-app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use(express.json())
 app.use(cors())
 
 app.get('/status', (req, res) => {
